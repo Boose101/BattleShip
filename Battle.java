a30a78d05756911264ba49cc02de7dda051531fe
Boose101 <51867298+Boose101@users.noreply.github.com>
2023-02-15 00:15:08 -0500

    Update Battle.java
    Base framework - Complete unoptimized.

--- Battle.java
+++ Battle.java
@@ -1,5 +1,3 @@
-import Wordle.main;
-
 import java.util.Scanner;
 public class Battle {
   
@@ -15,6 +13,7 @@ public class Battle {
     public static void main(String[] args) {
         String [][] shipBoard = new String[10][10];
         String [][] battleBoard = new String[10][10];
+        int[] statusArr = new int[5];
         Scanner scan = new Scanner(System.in);
         System.out.println("Welcome to BATTLESHIP!");
         System.out.println("Player one, place your ships!");
@@ -23,22 +22,74 @@ public class Battle {
         System.out.println(getShipBoard(shipBoard));
 
         //Inital
-
         int[] infoArr = new int[4];
 
         infoArr = getInfo(scan, "Place your Carrier(5).", 5);
+        placeShip(infoArr, "C", shipBoard);
 
-        
+        System.out.println(getShipBoard(shipBoard));
+        infoArr = getInfo(scan, "Place your Battleship(4).", 4);
+        placeShip(infoArr, "B", shipBoard);
+
+        System.out.println(getShipBoard(shipBoard));
+        infoArr = getInfo(scan, "Place your Destroyer(3).", 3);
+        placeShip(infoArr, "D", shipBoard);
+
+        System.out.println(getShipBoard(shipBoard));
+        infoArr = getInfo(scan, "Place your Submarine(3).", 3);
+        placeShip(infoArr, "S", shipBoard);
 
 
-        System.out.println("Place your Battleship(4).");
+        System.out.println(getShipBoard(shipBoard));
+        infoArr = getInfo(scan, "Place your Patrol Boat(2).", 2);
+        placeShip(infoArr, "P", shipBoard);
 
-        System.out.println("Place your Destroyer(3).");
+        //Battle
 
-        System.out.println("Place your Submarine(3).");
+        initialize(battleBoard);
+        System.out.println("Player 2, its up to you to find and destroy the 5 ships of player 1");
 
-        System.out.println("Place your Patrol Boat(2).");
+        boolean notWon = true;
+        int count = 0;
 
+        while(notWon){
+            System.out.println(getShipBoard(battleBoard));
+            System.out.print("Please enter the row and collum of your attack:");
+
+            String uAttack = scan.nextLine();
+            String[] uInputArr = uAttack.split("");
+            int uAttackRow = (uInputArr[0].toLowerCase().charAt(0) - 'a');
+            int uAttackCol = Integer.parseInt(uInputArr[1]) - 1;
+
+            int[] uAttackArr = {uAttackRow, uAttackCol};
+
+            hitShip(shipBoard, battleBoard, uAttackArr, statusArr);
+            count++;
+
+            int x = 0;
+            if(statusArr[0] == 5){
+                x++;
+            }
+            if(statusArr[1] == 4){
+                x++;
+            }
+            if(statusArr[2] == 3){
+                x++;
+            }
+            if(statusArr[3] == 3){
+                x++;
+            }
+            if(statusArr[4] == 2){
+                x++;
+            }
+
+            if(x==5){
+                notWon = false;
+            }
+        }
+
+        System.out.println("You did it!");
+        System.out.println("Your score was: " + count);
 
     }
 
@@ -70,30 +121,102 @@ public class Battle {
         boolean parameters = true;
         int uRow = 0;
         int uCol = 0;
-        while(parameters){
-            System.out.println(message);
-            String uInput  = scan.nextLine();
-            String[] uInputArr = uInput.split("");
-            uRow = (uInputArr[0].toLowerCase().charAt(0) - 'a');
-            uCol = Integer.parseInt(uInputArr[1]) - 1;
-            if(uRow <= 10 && uCol <= 10){
-                parameters = false;
-            }
-        }
-        parameters = true;
         int dirBuff = 0;
-        while(parameters){
-            System.out.println("Horizontaly(0) or Vertically(1)?");
-            dirBuff = Integer.parseInt(scan.nextLine());
-            if(dirBuff != 0 || dirBuff != 1){
-                parameters = false;
+        boolean fits = true;
+        while(fits){
+            parameters = true;
+
+            while(parameters){
+                System.out.println(message);
+                String uInput  = scan.nextLine();
+                String[] uInputArr = uInput.split("");
+                uRow = (uInputArr[0].toLowerCase().charAt(0) - 'a');
+                uCol = Integer.parseInt(uInputArr[1]) - 1;
+                if(uRow <= 10 && uCol <= 10){
+                    parameters = false;
+                }
+            }
+            parameters = true;
+            
+            while(parameters){
+                System.out.println("Horizontaly(0) or Vertically(1)?");
+                dirBuff = Integer.parseInt(scan.nextLine());
+                if(dirBuff == 0 || dirBuff == 1){
+                    parameters = false;
+                }
+            }
+            if((uRow + len < 10 && dirBuff == 1)){
+                fits = false;
+            }else if((uCol + len < 10 && dirBuff == 0)){
+                fits = false;
             }
         }
         int[] arr = {uRow, uCol, dirBuff, len}; 
         return arr;
     }
     
-    public static void placeShip(int[] arr, char symbol, String[][] shipBoard){
-        
+    public static void placeShip(int[] arr, String symbol, String[][] shipBoard){
+        shipBoard[arr[0]][arr[1]] = symbol;
+        if(arr[2] == 1){
+            for(int i = 1; i < arr[3]; i++){
+                shipBoard[arr[0]+i][arr[1]] = symbol;
+            }
+        }else{
+            for(int i = 1; i < arr[3]; i++){
+                shipBoard[arr[0]][arr[1]+i] = symbol;
+            }
+        }
+
+    }
+
+    public static void hitShip(String[][] shipBoard, String[][] battleBoard, int[] arr, int[] status){
+        if(shipBoard[arr[0]][arr[1]].equals("-")){
+            battleBoard[arr[0]][arr[1]] = "M";
+            System.out.println("You did not hit a ship!");
+        }else{
+            battleBoard[arr[0]][arr[1]] = "X";
+            System.out.println("Direct Hit!");
+            if(shipBoard[arr[0]][arr[1]].equals("C")){
+                status[0]++;
+            }else if(shipBoard[arr[0]][arr[1]].equals("B")){
+                status[1]++;
+            }else if(shipBoard[arr[0]][arr[1]].equals("D")){
+                status[2]++;
+            }else if(shipBoard[arr[0]][arr[1]].equals("S")){
+                status[3]++;
+            }else{
+                status[4]++;
+            }
+
+            if(status[0] == 5){
+                System.out.println("The Crusier has sunk!");
+            }
+            if(status[1] == 4){
+                System.out.println("The Battleship has sunk!");
+            }
+            if(status[2] == 3){
+                System.out.println("The Destroyer has sunk!");
+            }
+            if(status[3] == 3){
+                System.out.println("The Submarine has sunk!");
+            }
+            if(status[4] == 2){
+                System.out.println("The Patrol Boat has sunk!");
+            }
+        }
+    }
+
+}
+
+class Ships{
+    private String name;
+    private Boolean sunk;
+    public Ships(){
+        name = "john";
+        sunk = false;
+    }
+    public Ships(String na, Boolean su){
+        name = na;
+        sunk = su;
     }
 }
