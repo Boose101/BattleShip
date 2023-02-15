4036b3adc97580e765ab348e856a9bbb1402329d
Boose101 <51867298+Boose101@users.noreply.github.com>
2023-02-14 23:07:18 -0500

    fix

--- Battle.java
+++ Battle.java
@@ -22,6 +22,23 @@ public class Battle {
         initialize(shipBoard);
         System.out.println(getShipBoard(shipBoard));
 
+        //Inital
+
+        int[] infoArr = new int[4];
+
+        infoArr = getInfo(scan, "Place your Carrier(5).", 5);
+
+        
+
+
+        System.out.println("Place your Battleship(4).");
+
+        System.out.println("Place your Destroyer(3).");
+
+        System.out.println("Place your Submarine(3).");
+
+        System.out.println("Place your Patrol Boat(2).");
+
 
     }
 
@@ -46,5 +63,37 @@ public class Battle {
 
         return buff;
     }
+
+
+    //Row, Collum, Direction(0 H, 1 V), Length
+    public static int[] getInfo(Scanner scan, String message, int len){
+        boolean parameters = true;
+        int uRow = 0;
+        int uCol = 0;
+        while(parameters){
+            System.out.println(message);
+            String uInput  = scan.nextLine();
+            String[] uInputArr = uInput.split("");
+            uRow = (uInputArr[0].toLowerCase().charAt(0) - 'a');
+            uCol = Integer.parseInt(uInputArr[1]) - 1;
+            if(uRow <= 10 && uCol <= 10){
+                parameters = false;
+            }
+        }
+        parameters = true;
+        int dirBuff = 0;
+        while(parameters){
+            System.out.println("Horizontaly(0) or Vertically(1)?");
+            dirBuff = Integer.parseInt(scan.nextLine());
+            if(dirBuff != 0 || dirBuff != 1){
+                parameters = false;
+            }
+        }
+        int[] arr = {uRow, uCol, dirBuff, len}; 
+        return arr;
+    }
     
+    public static void placeShip(int[] arr, char symbol, String[][] shipBoard){
+        
+    }
 }
